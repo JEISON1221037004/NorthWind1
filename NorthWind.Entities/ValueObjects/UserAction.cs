f1e25b2f1efff85d0dfa03d784f80924ccfcf470
JEISON1221037004 <jeisonmanuyama@gmail.com>
2024-10-16 16:57:55 -0500

    useraction y northwind writers - modificacion y creacion

--- NorthWind.Entities/ValueObjects/UserAction.cs
+++ NorthWind.Entities/ValueObjects/UserAction.cs
@@ -1,9 +1,13 @@
-﻿namespace NorthWind.Entities.ValueObjects
+﻿
+namespace NorthWind.Entities.ValueObjects
 {
     public class UserAction(string user, string description)
+    public class UserAction(string User, string Description)
     {
         public DateTime CreateDateTime { get; } = DateTime.Now;
         public string user => user;
         public string description => description;
+        public string User => User;
+        public string Description => Description;
     }
 }
