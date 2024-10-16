376aba580d6d30959d500ddc297a5d2e85207198
JEISON1221037004 <jeisonmanuyama@gmail.com>
2024-10-16 16:59:19 -0500

    Actiondebug

--- NorthWind.Entities/ValueObjects/UserAction.cs
+++ NorthWind.Entities/ValueObjects/UserAction.cs
@@ -1,13 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace NorthWind.Entities.ValueObjects
 {
-    public class UserAction(string user, string description)
     public class UserAction(string User, string Description)
+    public class UserAction(string user, string description)
     {
         public DateTime CreateDateTime { get; } = DateTime.Now;
-        public string user => user;
-        public string description => description;
         public string User => User;
         public string Description => Description;
+        public DateTime CreatedDateTime { get; } = DateTime.Now;
+        public string User => user;
+        public string Description => description;
     }
 }
