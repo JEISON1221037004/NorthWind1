afcbcc7e7557b880f5675f3d06127bb5a2dc7a8f
JEISON1221037004 <jeisonmanuyama@gmail.com>
2024-10-16 17:10:49 -0500

    cambios

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,14 +1,24 @@
 ﻿using NorthWind.ConsoleApp.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
 
-IUserActionWriter writer = default;
+IUserActionWriter Writer = new FileWriter();
+var Builder = Host.CreateApplicationBuilder();
+Builder.Services.AddSingleton<IUserActionWriter, DebugWriter>();
+Builder.Services.AddSingleton<Applogger>();
+Builder.Services.AddSingleton<ProductService>();
+using var AppHost = Builder.Build();
 IUserActionWriter Writer = new ConsoleWriter();
 
-Applogger Logger = new Applogger(writer);
 Applogger Logger = new Applogger(Writer);
 Logger.WriteLog("Application Started.");
 
 
-ProductService service = new ProductService(Writer);
-service.Add("Demo", "Azucar refinada");
+ *Applogger : Inversio de dependencias, los modulos 
+ * de alto nivel son independientes de los detalles de implementacion
+ * 
+ * */
+ * */"Demo", "Azucar refinada");
