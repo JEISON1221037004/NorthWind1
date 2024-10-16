bbf898fa2a8324875d1467d212cd445e0bac6fdd
JEISON1221037004 <jeisonmanuyama@gmail.com>
2024-10-16 16:51:39 -0500

    Services, applogger y program

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NorthWind.ConsoleApp.Services;
+
 Console.WriteLine("Hello, World!");
+using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+Applogger Logger = new Applogger((IUserActionWriter)default);
+Logger.WriteLog("Application Started.");
